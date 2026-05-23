1cbbb840da016126d9f0e4d8bae2f86f0639adc2
saurabh <saurabhjain04r@gmail.com>
2026-05-23 19:13:07 +0530

    First Commit

---
+++ Low_Level_design/src/main/java/com/LLD/Low_Level_design/LowLevelDesignApplication.java
@@ -0,0 +1,13 @@
+package com.LLD.Low_Level_design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowLevelDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowLevelDesignApplication.class, args);
+	}
+
+}
---
+++ Low_Level_design/src/test/java/com/LLD/Low_Level_design/LowLevelDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LLD.Low_Level_design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowLevelDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
